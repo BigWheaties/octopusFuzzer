5eba46205514305ad21bb38acd6ff9c991078c8f
Michael Mason <michaelgrammason@gmail.com>
2021-12-07 07:39:54 -0600

    added part of Volumes fuzzing and other changes

--- octopusFuzzer.py
+++ octopusFuzzer.py
@@ -113,32 +113,120 @@ def fuzzDeployment(driver):
             maxUnavailableTarget.send_keys(fuzz(0, 50))
             maxSurgeTarget = driver.find_element_by_name("Max Surge")
             maxSurgeTarget.clear()
-            maxSurgeTarget.sendKeys(fuzz(0, 50))
+            maxSurgeTarget.send_keys(fuzz(0, 50))
     except:
         print("ERROR: DEPLOYMENT STRATEGY SELECTION FAILED")
         
     # add volumes
     try:
-        driver.find_element_by_xpath("//button[@title='Add Volume']").click() 
-        volumeType = random.randint(0, 5)
-        
-        # config map
-        if volumeType == 0:
+        numVolumes = random.randint(0, 5)
+        for i in range(numVolumes):
+            driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[2]/div[2]/div/div/div/div[3]/div[1]/div/button']").click() 
+            
+            volumeType = random.randint(0, 5)
+            # config map
+            if volumeType == 0:
+                configMapOption = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div/div/div[2]")
+                configMapOption.click()
+                configMapOption.send_keys(ENTER)
+                configMapName = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[2]/div[1]/div/div/div/input")
+                configMapName.clear()
+                configMapName.send_keys(fuzz(0, 50))
+                configMapNameName = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[1]/div[1]/div/div/div/input")
+                configMapNameName.clear()
+                configMapNameName.send_keys(fuzz(1, 50))
+                # add item
+                numItems = random.randint(0, 5)
+                for i in range(numItems):
+                    driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div/button").click()
+                    
+                    
+                    if i == 0:
+                        itemKeyTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[1]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                        itemKeyTarget.clear()
+                        itemKeyTarget.send_keys(fuzz(0, 50))
+                        itemPathTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[1]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                        itemPathTarget.clear()
+                        itemPathTarget.send_keys(fuzz(0, 50))
+                    
+                    elif i == 1:
+                        itemKeyTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[2]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                        itemKeyTarget.clear()
+                        itemKeyTarget.send_keys(fuzz(0, 50))
+                        itemPathTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[2]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                        itemPathTarget.clear()
+                        itemPathTarget.send_keys(fuzz(0, 50))
+                    elif i == 2:
+                        itemKeyTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[3]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                        itemKeyTarget.clear()
+                        itemKeyTarget.send_keys(fuzz(0, 50))
+                        itemPathTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[3]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                        itemPathTarget.clear()
+                        itemPathTarget.send_keys(fuzz(0, 50))
+                    elif i == 3:
+                        itemKeyTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[4]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                        itemKeyTarget.clear()
+                        itemKeyTarget.send_keys(fuzz(0, 50))
+                        itemPathTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[4]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                        itemPathTarget.clear()
+                        itemPathTarget.send_keys(fuzz(0, 50))
+                    elif i == 4:
+                        itemKeyTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[5]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                        itemKeyTarget.clear()
+                        itemKeyTarget.send_keys(fuzz(0, 50))
+                        itemPathTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[2]/div[2]/div[5]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                        itemPathTarget.clear()
+                        itemPathTarget.send_keys(fuzz(0, 50))
+            
+            driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[3]/div[2]/div[2]/button").click()
+            
+            ''' not implemented due to time constraints
+            # secret
+            elif volumeType == 1:
+                secretOption = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div/div/div[2]")
+                secretOption.click()
+                secretOption.send_keys(ARROW_DOWN)
+                secretOption.send_keys(ENTER)
 
-        # secret
-        elif volumeType == 1:
-        
-        # empty dir
-        elif volumeType == 2:
-        
-        # host path
-        elif volumeType == 3:
-
-        # persistent volume claim
-        elif volumeType == 4:
-
-        # raw YAML
-        elif volumeType == 5:    
+            # empty dir
+            elif volumeType == 2:
+                Option = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div/div/div[2]")
+                Option.click()
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ENTER)       
+                
+            # host path
+            elif volumeType == 3:
+                Option = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div/div/div[2]")
+                Option.click()
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ENTER)
+                
+            # persistent volume claim
+            elif volumeType == 4:
+                Option = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div/div/div[2]")
+                Option.click()
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ENTER)
+                
+            # raw YAML
+            elif volumeType == 5: 
+                Option = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[1]/div/div/div/div[2]")
+                Option.click()
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ARROW_DOWN)
+                Option.send_keys(ENTER)   
+            '''
+            
     except:
         print("ERROR: VOLUME ADDITION FAILED")
         
@@ -161,50 +249,108 @@ def fuzzDeployment(driver):
     
     # manage DNS config
     try:
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        nameServerTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[1]/div[1]/div/div/div/textarea[1]")
+        nameServerTarget.clear()
+        nameServerTarget.send_keys(fuzz(0, 50))
+        searchesTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[3]/div[1]/div/div/div/textarea[1]")
+        searchesTarget.clear()
+        searchesTarget.send_keys(fuzz(0, 50))
+        try:
+            numOptions = random.randint(0, 5)
+            if numOptions != 0:
+            for i in range(numOptions):
+                driver.find_element_by_xpath("").click() 
+                    
+                if i == 0:
+                    optionNameTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[1]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                    optionNameTarget.clear()
+                    optionNameTarget.send_keys(fuzz(0, 50))
+                    optionValueTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[1]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                    optionValueTarget.clear()
+                    optionValueTarget.send_keys(fuzz(0, 50))
+                    
+                elif i == 1:
+                    optionNameTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[2]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                    optionNameTarget.clear()
+                    optionNameTarget.send_keys(fuzz(0, 50))
+                    optionValueTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[2]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                    optionValueTarget.clear()
+                    optionValueTarget.send_keys(fuzz(0, 50))
+                        
+                elif i == 2:
+                    optionNameTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[3]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                    optionNameTarget.clear()
+                    optionNameTarget.send_keys(fuzz(0, 50))
+                    optionValueTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[3]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                    optionValueTarget.clear()
+                    optionValueTarget.send_keys(fuzz(0, 50))                   
+                elif i == 3:
+                    optionNameTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[4]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                    optionNameTarget.clear()
+                    optionNameTarget.send_keys(fuzz(0, 50))
+                    optionValueTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[4]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                    optionValueTarget.clear()
+                    optionValueTarget.send_keys(fuzz(0, 50))                    
+                elif i == 4:
+                    optionNameTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[5]/div/div[1]/div[1]/div/div[1]/div/div/div/input")
+                    optionNameTarget.clear()
+                    optionNameTarget.send_keys(fuzz(0, 50))
+                    optionValueTarget = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[5]/div[2]/div/div/div/div[5]/div[2]/div[5]/div/div[1]/div[2]/div/div[1]/div/div/div/input")
+                    optionValueTarget.clear()
+                    optionValueTarget.send_keys(fuzz(0, 50))
+            except:
+                print("ERROR: FAILED TO PROPERLY CREATE OPTIONS IN DNS CONFIG")
     except:
         print("ERROR: DNS CONFIG FAILED")
     
     # manage host networking
     try:
-        # ! - difficult selection process, TODO
+        hostNetworkingInt = random.randint(0, 1)
+        if hostNetworkingInt == 0:
+            hostNetworkingChoice = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[6]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[1]/input")
+        if hostNetworkingInt == 1:
+            hostNetworkingChoice = driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[6]/div[2]/div/div/div/div[1]/div[1]/div[2]/div[3]/input")
+        hostNetworkingChoice.click()
     except:
         print("ERROR: FAILED TO PROPERLY SELECT A HOST NETWORKING OPTION")
 
     # manage pod security context
-    try:
+    #try:
         # ! - TODO, VERY COMPLEX
-    except:
-        print("ERROR: POD SECURITY CONTEXT CONFIGURATION FAILED")
+    #except:
+    #    print("ERROR: POD SECURITY CONTEXT CONFIGURATION FAILED")
     
     # manage pod affinity/anti-affinity
-    try:
+    #try:
         # ! - TODO, VERY COMPLEX
-    except:
-        print("ERROR: POD AFFINITY CONFIGURATION FAILED")
+    #except:
+    #    print("ERROR: POD AFFINITY CONFIGURATION FAILED")
     
     # manage node affinity
-    try:
+    #try:
         # ! - TODO, VERY COMPLEX
-    except:
-        print("ERROR: NODE AFFINITY CONFIGURATION FAILED")
+    #except:
+    #    print("ERROR: NODE AFFINITY CONFIGURATION FAILED")
     
     # manage tolerations
     try:
-        # ! - TODO, VERY COMPLEX
+        numTolerations = random.randint(0, 5)
+        for i in range(numTolerations):
+            driver.find_element_by_xpath("/html/body/div[1]/div/div[2]/div/div[10]/div[2]/div/div/div/div/div[1]/div/button").click()
+            keyTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[2]/div[1]/div[1]/div/div/div/input")
+            keyTarget.clear()
+            keyTarget.send_keys(fuzz(0, 50))
+            operatorTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[3]/div[1]/div[1]/div/div/div/input")
+            operatorTarget.clear()
+            operatorTarget.send_keys(fuzz(0, 50))
+            tolerationValueTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[4]/div[1]/div[1]/div/div/div/input")
+            tolerationValueTarget.clear()
+            tolerationValueTarget.send_keys(fuzz(0, 50))
+            effectTarget = driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[2]/div/div/div/div[5]/div[1]/div[1]/div/div/div/input")
+            effectTarget.clear()
+            effectTarget.send_keys(fuzz(0, 50))
+            driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div/div[3]/div[2]/div[2]/button").click()
+            
     except:
         print("ERROR: TOLERATION CONFIGURATION FAILED")
     
